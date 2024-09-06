6849b0beeb58895f5433391acd6e4630f350525d
echavarriasam <echavarriasam@gmail.com>
2024-09-06 15:16:22 +0000

    update widget id

--- src/components/power-bi-widget/power-bi-widget.tsx
+++ src/components/power-bi-widget/power-bi-widget.tsx
@@ -7,7 +7,7 @@ export const PowerBIWidget: React.FC = (): JSX.Element => {
         id="SICR_Datos"
         className={Css.frame}
         title="SICR_Datos"
-        src="https://app.powerbi.com/reportEmbed?reportId=b5a6119f-c0f8-4028-a519-4d5eb81d33eb&autoAuth=true&embeddedDemo=true"
+        src="https://app.powerbi.com/reportEmbed?reportId=b5a6119f-c0f8-4028-a519-4d5eb81d33eb&autoAuth=true&ctid=643c354c-8e33-43b1-9a6b-363c52959b4b"
         frameBorder={0}
         allowFullScreen
       />
