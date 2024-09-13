ae09fb71511ef72acd881e3d7008818841282f8a
echavarriasam <echavarriasam@gmail.com>
2024-09-13 17:28:29 +0000

    fix widget

--- src/components/power-bi-widget/power-bi-widget.tsx
+++ src/components/power-bi-widget/power-bi-widget.tsx
@@ -7,7 +7,7 @@ export const PowerBIWidget: React.FC = (): JSX.Element => {
         id="SICR_Datos"
         className={Css.frame}
         title="SICR_Datos"
-        src="https://app.powerbi.com/view?r=eyJrIjoiODcwYTJiMjEtZDFhMy00YTMwLWIzMDItZTE5NTBjMWU0NGEzIiwidCI6IjY0M2MzNTRjLThlMzMtNDNiMS05YTZiLTM2M2M1Mjk1OWI0YiIsImMiOjR9"
+        src="https://app.powerbi.com/view?r=eyJrIjoiODcwYTJiMjEtZDFhMy00YTMwLWIzMDItZTE5NTBjMWU0NGEzIiwidCI6IjY0M2MzNTRjLThlMzMtNDNiMS05YTZiLTM2M2M1Mjk1OWI0YiIsImMiOjR9&pageName=c833276eb095ee00c6bd"
         frameBorder={0}
         allowFullScreen
       />
