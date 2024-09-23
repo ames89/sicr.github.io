5011fef577835e7a0783a9d85829bfe82d99a584
echavarriasam <echavarriasam@gmail.com>
2024-09-23 00:52:35 +0000

    fixes

--- src/components/content6/content.tsx
+++ src/components/content6/content.tsx
@@ -8,24 +8,20 @@ export const Content6 = () => {
       <div className={css.cubes} />
       <div className={css.container1_1}>
         <div className={css.container2}>
-          <h2 className={css.header1}>
-            Mas valiosa que el dinero, es la información
-          </h2>
-          <p className={css.p}>
-            ¡Únete a nuestro proyecto y contribuye al futuro sostenible de Costa
-            Rica! Necesitamos tus datos sobre la generación de residuos para
-            optimizar su gestión mediante inteligencia artificial. Con tu ayuda,
-            podemos reducir desechos, promover la economía circular y crear un
-            impacto positivo en nuestra comunidad y el medio ambiente.
-            ¡Participa!
-          </p>
           <h2 className={css.header2}>Para más información</h2>
           <a
-            href={MailtoContent}
+            href="/files/Resumen_Ejecutivo_compressed.pdf"
             target="_blank"
             className={joinClasses("button", css.button)}
           >
-            Solicitar información
+            Resumen Ejecutivo
+          </a>
+          <a
+            href="/files/Analisis_de_Resultados_SICR_compressed.pdf"
+            target="_blank"
+            className={joinClasses("button", css.button)}
+          >
+            Análisis de los Resultados
           </a>
         </div>
       </div>
@@ -35,8 +31,12 @@ export const Content6 = () => {
           <a className={css.link} href="#top">
             Principal
           </a>
-          <a className={css.link} href="#acerca">
-            Acerca
+          <a
+            target="_blank"
+            className={css.link}
+            href="https://app.powerbi.com/view?r=eyJrIjoiODcwYTJiMjEtZDFhMy00YTMwLWIzMDItZTE5NTBjMWU0NGEzIiwidCI6IjY0M2MzNTRjLThlMzMtNDNiMS05YTZiLTM2M2M1Mjk1OWI0YiIsImMiOjR9&pageName=c833276eb095ee00c6bd"
+          >
+            SICR
           </a>
           <a target="_blank" className={css.link} href={MailtoContent}>
             Contacto
